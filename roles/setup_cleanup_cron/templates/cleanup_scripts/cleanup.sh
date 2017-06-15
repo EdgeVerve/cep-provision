d9f009043d9fdbcb531d9b7437ef9615cf87bb73
ajith <ajith_v@edgeverve.com>
2017-06-15 16:47:20 +0530

    Fixed thard-coded path with template variable

--- roles/setup_cleanup_cron/templates/cleanup_scripts/cleanup.sh
+++ roles/setup_cleanup_cron/templates/cleanup_scripts/cleanup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Clean up docker engine - images, containers, networks, volumes
 
-docker system prune -fa >> /datadisk/evfpaas/cleanup_prune.log
+docker system prune -fa >> /{{cepfolder}}/cep/cleanup_prune.log
 
 #clean up registry data
 
-docker exec -it `docker ps | grep private_registry | awk '{print $1}' | head -1` /bin/registry garbage-collect /etc/docker/registry/config.yml >> /datadisk/evfpaas/cleanup_registry.log
+docker exec -it `docker ps | grep private_registry | awk '{print $1}' | head -1` /bin/registry garbage-collect /etc/docker/registry/config.yml >> /{{cepfolder}}/cep/cleanup_registry.log
